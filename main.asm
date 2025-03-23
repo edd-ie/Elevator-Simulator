2c68e0ea52577b9b56b74ae969f1746df24b671a
edd-ie <ogendoeddie@gmail.com>
2025-03-23 10:49:21 -0300

    Keyboard numerical filtering

--- main.asm
+++ main.asm
@@ -10,10 +10,18 @@ main:
 run:
 	# Reading keyboard input
 	li $t0, 0                                  #initialize character to zero
-	GetCharacter($t0, $v0)                     #wait for user to enter in character
+	GetCharacter($t0, $v0)                     #wait for user to enter in character	
+	
 	move $t0, $v0                              #store character in $t0	
+	
+	li $t3, '0'
+	li $t4, '9'
+	
+	blt $t0, $t3, Skip
+	blt $t4, $t0, Skip
 	print($t0)
 	
+	Skip:
 	li $t1, esc
 	bne $t0, $t1, run
 
